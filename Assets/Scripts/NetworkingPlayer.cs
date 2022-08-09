9dd935d44cb347c0bd24cb6d316864ea1733a3a6
Emily <emidude@gmail.com>
2022-08-09 05:41:25 +0100

    changed head hookup

--- Assets/Scripts/NetworkingPlayer.cs
+++ Assets/Scripts/NetworkingPlayer.cs
@@ -72,7 +72,8 @@ public class NetworkingPlayer : NetworkBehaviour
 
         // now link localHMD, localHands to the Rig so that they are
         // automatically filled when the rig moves
-        localHead = Camera.main.gameObject; // get HMD
+        //localHead = Camera.main.gameObject; // get HMD
+        localHead = theLocalPlayer.transform.Find("FolowHead").gameObject;
         Debug.Log("local head = " + localHead);
         //localLeftHand = theLocalPlayer.transform.Find("LeftHand").gameObject;
         localLeftHand = GameObject.FindWithTag("LeftHand");
